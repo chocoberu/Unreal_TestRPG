e6ab08fa5af03c85197ebc717b6efd87d86828e9
chocobe <mjh2903@naver.com>
2021-03-26 11:14:10 +0900

    에셋 추가

---
+++ Source/TestRPG/Public/PlayerCharacter.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "PlayerCharacter.generated.h"
+
+UCLASS()
+class TESTRPG_API APlayerCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	APlayerCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Input Func
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+
+	// Camera
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	class UCameraComponent* CameraComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	class USpringArmComponent* SpringArmComp;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
---
+++ Source/TestRPG/Public/TwinGunnerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PlayerCharacter.h"
+#include "TwinGunnerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTRPG_API ATwinGunnerCharacter : public APlayerCharacter
+{
+	GENERATED_BODY()
+	
+};
